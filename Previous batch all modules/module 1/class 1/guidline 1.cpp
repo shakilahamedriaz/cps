79815d79e7391fe7959fb6dff9d9403dd20df6e8
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-05-16 20:25:00 +0600

    go on

---
+++ Previous batch all modules/module 1/class 1/guidline 1.cpp
@@ -0,0 +1,10 @@
+// Mindset, Mindset and Mindset.
+// Practice regularly.
+// Understand fundamentals.
+// Analyze problems.
+// Break down problems.
+// Use efficient algorithms.
+// Optimize code.
+// Learn from others.
+// Stay calm under pressure.
+// Review and reflect.
