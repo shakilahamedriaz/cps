eb69c29afaf5c45de84bcde242fa563760e11b38
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-07-04 01:56:12 +0600

    about datType

---
+++ Previous batch all modules/module 1/class 8/task1.cpp
@@ -0,0 +1,45 @@
+** float data type
+..........................      
+
+- to store fractional number we use float or double
+- in cp , double should be used for safe operation.
+- %f - format spicifier in c;
+- now we are learn about double
+
+**double data type :
+...........................
+- double date type size 8 byte (64 bits).
+- precession, typecasting etc
+- %d - format spicifier in c;
+- double(x)
+- 5/(3 * 1.0);
+- 5.0/3; etc way
+- int(ans);
+
+
+**long long data type :
+..........................
+
+-long long dataype 8 byte
+- range - 9*10^18 to 9*10^18
+- %lld - format spicifier in c;
+
+**unsigned means:
+....................
+
+- without any signed(+/-), means unsigned ;
+
+
+**char data type:
+.......................
+
+- used to store character
+- size 1 byte;
+- character input nile space na dileo hoy.
+
+
+
+
+
+
+
