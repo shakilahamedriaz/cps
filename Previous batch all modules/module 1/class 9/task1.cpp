87591626e2575cc8ee344f4467b6c8e588572878
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-07-06 02:09:02 +0600

    done

---
+++ Previous batch all modules/module 1/class 9/task1.cpp
@@ -0,0 +1,28 @@
+** remember
+..................
+- peq =  p * 10 ^q
+-pe-q  = p * 10 ^-q
+
+
+* to typecast someting into long long int 
+  -(long long int)variable
+  -1LL * variable
+* to typecast something into int
+    -(int)variable
+    -1 * variable
+* to typecast something into double
+    -(double)variable
+    -1.0 * variable
+* to typecast something into float
+    -(float)variable
+    -1.0f * variable
+* to typecast something into char
+    -(char)variable
+* to typecast something into int
+    -(int)variable
+    -1*variable
+
+** remember if any overflow
+
+** fast operaton + (+,-,*)
+** expensive operation  / & %
