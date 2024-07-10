3071b758451d20bb0ac9cd5d16f1aafbd626ddaf
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-07-11 03:08:50 +0600

    done

--- Previous batch all modules/module 1/class 10/task 2.cpp
+++ Previous batch all modules/module 1/class 10/task 2.cpp
@@ -1,4 +1,25 @@
 ** Separate the digits of a decimal number using the mod operation :
 .....................................................................
 
-- 10 min done
+- prottek ta number er last digit bade baki gula 10 dara divisibile.
+- 123 % 10  korle last digit hisebe 3 ber kore at the same time
+- 123 / 10 korle 12 hobe, agian 12 % 10 korle 2 ber kore
+- 12 / 10 korle 1 hobe, agian 1 % 10 korle 1 ber kore, 
+- eivabe e process continue hobe jabtak number 0 na hoiya jay.
+- and then print the number as reverse order, exam: 123 = 321
+
+
+# to find last digit of a number: number % 10
+# to remove last digit of a number: number / 10
+
+#SUPPOSE: number = 65634
+
+# 65634 % 10 = 4        ARRAY = 4
+# 65634 / 10 = 6563 
+# 6563 % 10 = 3         ARRAY = 4, 3
+# 6563 / 10 = 656
+# 656 % 10 = 6          ARRAY = 4, 3, 6
+# 656 / 10 = 65
+# 65 % 10 = 5           ARRAY = 4, 3, 6, 5
+# 65 / 10 = 6
+# 6 % 10 = 6            ARRAY = 4, 3, 6, 5, 6
---
+++ Previous batch all modules/module 1/class 10/task 3.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   
+   int number = 231;        // number = 231
+   
+   int c = number % 10;     /// c = 1
+   number = number / 10;    /// number = 23
+
+   int b = number % 10;    ///  b = 3
+   number = number / 10;   ///  number = 2
+
+   int a = number % 10;    ///  a = 2
+    
+
+
+   int abc = 0;             /// abc  = 0;
+
+   abc = abc + a;           /// abc= 0 + 2 = 2;
+   abc = abc * 10;          /// abc = 2 * 10 = 20
+
+   abc = abc + b;           /// abc = 20 + 3 = 23
+   abc = abc * 10;          /// abc = 23 * 10 = 230
+
+   abc = abc + c;           /// abc = 230 + 1 = 231
+
+
+   int bca = 0;             /// bca = 0;
+
+   bca = bca + b;           /// bca = 0 + 3;
+   bca = bca * 10;          /// bca = 3 * 10 = 30
+
+   bca = bca + c;           /// bca = 30 + 1;
+   bca = bca * 10;          /// bca = 31 * 10 = 310
+
+   bca = bca + a;           /// bca = 310 + 2 = 312
+
+
+   int cab = 0;             /// cab = 0;
+
+   cab = cab + 1;           /// cab = 0 + 1 = 1
+   cab = cab * 10;         /// cab = 1 * 10 = 10
+
+   cab = cab + a;         /// cab = 10 + 2 = 12
+   cab = cab * 10;        /// cab = 12 * 10 = 120
+
+   cab = cab + b;         // cab = 120 + 3 = 123
+
+
+   int ans = abc + bca + cab;
+
+   cout<< ans << endl;
+
+
+    return 0;
+}
