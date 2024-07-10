5c764db962c6fdabf411c9f30f589d4e0c5c17c8
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-07-11 02:23:26 +0600

    to find each digits of numbers

---
+++ Previous batch all modules/module 1/class 10/task 1.cpp
@@ -0,0 +1,9 @@
+** 1. Modulus Operations :
+.................................
+
+-Modulus / mod  , means reminder of division of two numbers.
+-For example, 5 % 2 = 1, because 5 divided by 2 is 2 and reminder is 1.
+-ex : 23 / 5 = 4 and reminder is 3.
+- 20 + 3 = 23 or we can explain it, 5 * 4 + 3 = 23., extra 3 is reminder.
+- 23 % 5 = 3.
+
---
+++ Previous batch all modules/module 1/class 10/task 2.cpp
@@ -0,0 +1,4 @@
+** Separate the digits of a decimal number using the mod operation :
+.....................................................................
+
+- 10 min done
