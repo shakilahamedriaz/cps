13131760cfdc6ff06c0f91f0492d9ebf07c54ede
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-07-15 18:42:55 +0600

    done

---
+++ Previous batch all modules/module 1/class 11/task 1.cpp
@@ -0,0 +1,44 @@
+** 🎯 Discussed topics:
+1. More operators (+=, -=, =, /=, %= )
+2. Increment, decrement
+3. Pre increment / decreament
+4. Post increment / decreament
+5. Logical operators OR(||), AND(&&)
+
+
+
+# 1 : More operators (+=, -=, =, /=, %= ) :
+.............................................
+- learn them
+
+# 2 : Increment, decrement :
+.............................................
+
+- Increment : increase the value of a variable by 1
+- Decrement : decrease the value of a variable by 1
+
+- Increment : ++ (before or after the variable)
+- Decrement : -- (before or after the variable)
+
+- Example : 
+int x = 5;
+x++; // x = 6
+x--; // x = 5
+
+# 3 : Pre increment / decreament :
+.............................................
+
+- Pre increment : ++x // 1st increase then save
+- Pre decrement : --x // 1st decrease then save
+
+Example : 
+
+ a = 12;
+ b = --a;
+
+ print : a space b;
+ output : 
+
+ explaintion :  
+
+ 
