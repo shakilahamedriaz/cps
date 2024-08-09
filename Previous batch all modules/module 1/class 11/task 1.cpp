d9d0517cb6a83b091b44de0203808b6b412230e1
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-08-10 01:52:39 +0600

    learned increment, decrement

--- Previous batch all modules/module 1/class 11/task 1.cpp
+++ Previous batch all modules/module 1/class 11/task 1.cpp
@@ -1,4 +1,5 @@
 ** 🎯 Discussed topics:
+
 1. More operators (+=, -=, =, /=, %= )
 2. Increment, decrement
 3. Pre increment / decreament
@@ -25,20 +26,63 @@ int x = 5;
 x++; // x = 6
 x--; // x = 5
 
-# 3 : Pre increment / decreament :
+# 3 : Pre/ post --> increment :
 .............................................
 
 - Pre increment : ++x // 1st increase then save
 - Pre decrement : --x // 1st decrease then save
+- post increment : x ++ // save then increase
+- post decreament : x -- // save then decrease 
 
-Example : 
+Example 1: 
+..........
 
- a = 12;
- b = --a;
+a = 12;
+b = a++;  //save then increase
 
- print : a space b;
- output : 
+print (a, b);
+output : 13, 12
 
- explaintion :  
+Example 2 : 
+...........
 
- 
+a = 12;
+b = ++ a;  // increase then save
+
+print (a, b);
+output : 13, 13
+
+
+
+# 4 : Pre/ post -->  decreament :
+..................................
+
+Example 1;
+
+a = 12;
+c = a--;  //save then decrease
+
+print (a, c);
+output : 11, 12
+
+Example 2 :
+
+a = 12;
+c = -- a;  // decrease then save
+
+print (a, c);
+output : 11, 11
+
+
+
+
+
+Example 3 : (both pre and post increment / decrement) :
+.........................................................
+
+a = 12, b = 12;
+c = --a;
+d = b--;
+
+print (a, b, c, d);
+output : 11, 11, 11, 12
---
+++ Previous batch all modules/module 1/class 11/task 2.cpp
@@ -0,0 +1,27 @@
+// Bolean data type 1 byte;
+
+Example 1;
+
+bool a = true;
+bool b = false;
+
+print (a, b);
+output : 1, 0
+
+
+Example 2;
+
+bool a = (15 < 10);
+print a;
+
+output : 0
+
+
+
+
+# 5 logical operator : 
+......................
+
+or = ||
+and = &&
+not = !
