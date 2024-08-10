a6e5f30067e4fdb1951665aaa18a474b2c1a5f5c
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-08-10 23:21:35 +0600

    Bitwise Op

---
+++ Previous batch all modules/module 1/class 12/Bitwise AND.cpp
@@ -0,0 +1,37 @@
+# Bitwise AND Operations:
+............................
+# 2.  Bitwise operators AND(&):
+
+example 1 : 
+.............
+3 and 4
+3 & 4
+
+3 = 011
+4 = 100
+0 = 000 = 0 (decimal)
+
+
+*here is code :
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 3, b = 4;
+    cout << (a & b) << endl;  // output: 0
+    return 0;
+}
+
+
+
+example 2 :
+............
+
+3 & 5
+
+3 = 011
+5 = 101
+1 = 001 = 1 (decimal)
+
+
+
---
+++ Previous batch all modules/module 1/class 12/Bitwise Xor.cpp
@@ -0,0 +1,43 @@
+# Bitwise AND Operations:
+............................
+# 3 Bitwise operators XOR(^): bit same hole 0, otherwise 1
+                             // odd number 1 thakle 1. even number 0 thakle 0
+
+17 XOR 12
+
+17 = 10001
+12 = 01100
+...............
+29 = 11101   
+
+*here is the code : 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 17, b = 12;
+    int ans = a ^ b;
+    cout << ans << endl;  // output: 29
+}
+
+
+Note : If both digits are same then xor value will 0;
+
+if 5 ^ 5 = 0, cz 5 = 101, 5 = 101, 0 = 000
+if 5 ^ 5 ^ 5 = 5;
+if 5 ^ 5 ^ 3 = 3; five-five vanish, then 3 will exist.
+
+
+example 2 : 
+
+3 ^ 5 ^ 10;
+
+3  = 0011
+5  = 0101
+10 = 1010
+..............
+12 = 1100  ans.
+
+
+// https://drive.google.com/file/d/1wNPuFbyqpQkFp4JQEoLu4VSeZ0oCJ94H/view
