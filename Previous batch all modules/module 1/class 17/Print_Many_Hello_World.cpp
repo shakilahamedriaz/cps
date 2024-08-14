965273f60aa0bc0fdcf03ced29b9c54915136aec
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-08-14 18:38:14 +0600

    practise problem

---
+++ Previous batch all modules/module 1/class 17/Print_Many_Hello_World.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t = 1000;
+    while( t-- )
+    {
+        cout<< "Hello World" << "\n";
+    }
+
+    return 0;
+}
---
+++ Previous batch all modules/module 1/class 17/Print_Test_Cases.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   
+    int i = 1;
+    while( 1 )
+    {
+        int n;
+        cin >> n;
+        if(n == 0)
+        {
+            break;
+        }
+        cout<< "Case " << i << ": " << n << "\n";
+        i++;
+    }
+
+    return 0;
+}
